9b0c5616ed21501353061012ab32300f11ed7061
Kyle Jorgensen <kylewjorgensen@gmail.com>
2014-10-21 00:29:59 -0700

    initial round trip time calculations

--- hw1/clock_sync.py
+++ hw1/clock_sync.py
@@ -20,22 +20,29 @@ while(True):
   try:
     msg = 'time' # this message doesn't matter, it just needs to be non-empty
 
-    #print("Getting time...")
-    # Set the whole string
-    s.sendto(msg.encode('utf-8'), (host3, port))
+    # Semd the request to the server
+    t_send = time.clock_gettime(time.CLOCK_REALTIME)
+    s.sendto(msg.encode('utf-8'), (host1, port))
+
+    t_local = time.clock_gettime(time.CLOCK_REALTIME)
 
     # receive data from time server 1 (data, addr)
     d = s.recvfrom(1024) # buffer size 1024 bytes
-    reply = d[0].decode("utf-8")
-    addr = d[1]
-    
-    reply_date = float(reply)
+    t_recv = time.clock_gettime(time.CLOCK_REALTIME)
+    t_round = t_recv - t_send
 
-    print ('Server reply : ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(reply_date)))
+    reply = d[0].decode("utf-8")
+    reply_float = float(reply)
+
+    #print ('Server reply: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(reply_float)) )
+    print("RTT: " + str(t_round))
+    print("Diff: " + str( t_local - (reply_float + t_round/2)) + '\n')
+    #print("current time: " + str(time.clock_gettime(time.CLOCK_REALTIME)) + " server w/ adj: " 
+     #     + str(reply_float + t_round/2))
 
   # some problem seding data?
   except socket.error as e:
-    print ('Error Code : ' + str(e[0]) + ' Message ' + e[1])
+    print ('Error Code: ' + str(e[0]) + ' Message ' + e[1])
     sys.exit()
 
   # ctrl-C
