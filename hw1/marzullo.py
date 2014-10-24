dcfe4e5fae3a27683098faa0bb56657af62a8702
Kyle Jorgensen <kylewjorgensen@gmail.com>
2014-10-24 01:30:24 -0700

    added my new Marzullo improvement if there are no overlapping intervals, then take the two that are the closest, and return the interval between these two interval midpoints

--- hw1/marzullo.py
+++ hw1/marzullo.py
@@ -1,3 +1,6 @@
+# Kyle Jorgensen, CS 271, HW 1, 10/23/14
+# This code is built upon https://gist.github.com/tai2/3684493
+
 # c.f. http://en.wikipedia.org/wiki/Marzullo%27s_algorithm
 def marzullo_algorithm(ranges):
   table = []
@@ -20,9 +23,24 @@ def marzullo_algorithm(ranges):
       best = cnt
       beststart = table[i][0]
       bestend   = table[i+1][0]
-      
   
-  return (beststart, bestend)
+  # if we had overlapping intervals, return the Marzullo answer
+  if best > 1:  
+    return (beststart, bestend)
+  
+  else: # Otherwise, find the two intervals that were closest 
+        # and take the midpoints of those two as the result
+    cnt = 0
+    diff = 9999999 # some large integer
+    for i in range(1, len(table)-1, 2):
+      cnt = abs(table[i][0] - table[i+1][0])
+      if cnt < diff:
+        diff = cnt
+        mid1 = (table[i-1][0] + table[i][0])/2
+        mid2 = (table[i+1][0] + table[i+2][0])/2
+
+    return (mid1, mid2)
+
     
 def test(data_list):
   for data in data_list:
@@ -36,11 +54,12 @@ if __name__ == "__main__":
   test_data_list = (
       {'input' : ((8,12),(11,13),(10,12)), 'expected' : (11,12)},
       {'input' : ((8,12),(11,13),(14,15)), 'expected' : (11,12)},
-      {'input' : ((8,9),(8,12),(10,12)), 'expected' : (8,9)},
+      {'input' : ((8,9),(8,12),(10,12)), 'expected' : (10,12)},
       {'input' : ((8,12),(9,10),(11,13),(10,12)), 'expected' : (11,12)},
-      {'input' : ((8,12),(9,10),(11,13),(14,15)), 'expected' : (9,10)},
-      {'input' : ((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15)), 'expected' : (12,13)})
+      {'input' : ((8,12),(9,10),(11,13),(14,15)), 'expected' : (11,12)},
+      {'input' : ((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15)), 'expected' : (12,13)},
+      {'input' : ((-13.0,-11.0),(-6.2,-6.0),(-2.4,-2.2),(4.8,5.0),(6.0,6.4)), 'expected' : (4.9, 6.2)},
+      {'input' : ((8.0,9.0),(10.0,11.0),(12.0,13.0)), 'expected' : (8.5,10.5) },
+      {'input' : ((1.0,2.0),(2.0,3.0),(4.0,5.0)), 'expected' : (1.5,2.5) })
   test(test_data_list)
-  test_list = ( (-13.0,-11.0),(-6.2,-6.0),(-2.4,-2.2),(4.8,5.0),(6.0,6.4))
-  result = marzullo_algorithm(test_list)
-  print(result)
+ 
