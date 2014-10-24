faad4b00291fb76a95550f1ab0dcc5ba73adb81e
Kyle Jorgensen <kylewjorgensen@gmail.com>
2014-10-24 00:43:55 -0700

    CHECKPOINT - communication with all five servers working also the marzullo is working still need to shift times for comparison also need to add logic to Marzulo for no overlap

--- hw1/clock_sync.py
+++ hw1/clock_sync.py
@@ -4,6 +4,7 @@
 import socket
 import sys
 from datetime import datetime, timedelta
+import marzullo
 
 host1 = '54.169.67.45'
 host2 = '54.207.15.207'
@@ -25,35 +26,93 @@ def sync():
       msg = 'time' # this message doesn't matter, it just needs to be non-empty
 
       # Send the request to the server
-      t_send = datetime.now()
-      s.sendto(msg.encode('utf-8'), (host5, port2))
-
-      t_local = datetime.now()
+      t_send_1 = datetime.now()
+      s.sendto(msg.encode('utf-8'), (host1, port))
 
       # receive data from time server (data, addr)
-      d = s.recvfrom(1024) # buffer size 1024 bytes
-      t_recv = datetime.now()
+      data_1 = s.recvfrom(1024) # buffer size 1024 bytes
+      t_recv_1 = datetime.now()
 
       # Round trip time (timedelta obj)
-      t_round = t_recv - t_send
-      rtt = t_round.total_seconds()
+      t_round_1 = t_recv_1 - t_send_1
+      rtt_1 = t_round_1.total_seconds()
 
       # The time received from the server
-      reply = d[0].decode("utf-8")
-      reply_float = float(reply)
-      t_server = datetime.fromtimestamp(reply_float)
+      reply_float_1 = float(data_1[0].decode("utf-8"))
+      t_server_1 = datetime.fromtimestamp(reply_float_1)
 
       # time reported from the server plus RTT/2
-      t_server_corrected = t_server + timedelta(seconds=(rtt/2))
+      t_server_1_corrected = t_server_1 + timedelta(seconds=(rtt_1/2))
 
-      print ('Server reply: ' + str(t_server) )
-      print("RTT: " + str(rtt) )
-      print("**local time: " + str(t_recv) )
-      #print("**reply float: " + str( t_server ) )
-      print("Diff: " + str( (t_local - t_server_corrected).total_seconds() ) + '\n')
-      #print("current time: " + str(time.clock_gettime(time.CLOCK_REALTIME)) + " server w/ adj: " 
-       #     + str(reply_float + t_round/2))
+      # Get times from servers 2 - 5
+      t_send_2 = datetime.now()
+      s.sendto(msg.encode('utf-8'), (host2, port))
+      data_2 = s.recvfrom(1024) 
+      t_recv_2 = datetime.now()
+      t_round_2 = t_recv_2 - t_send_2
+      rtt_2 = t_round_2.total_seconds()
+      reply_float_2 = float(data_2[0].decode("utf-8"))
+      t_server_2 = datetime.fromtimestamp(reply_float_2)
+      t_server_2_corrected = t_server_2 + timedelta(seconds=(rtt_2/2))
 
+      t_send_3 = datetime.now()
+      s.sendto(msg.encode('utf-8'), (host3, port))
+      data_3 = s.recvfrom(1024) 
+      t_recv_3 = datetime.now()
+      t_round_3 = t_recv_3 - t_send_3
+      rtt_3 = t_round_3.total_seconds()
+      reply_float_3 = float(data_3[0].decode("utf-8"))
+      t_server_3 = datetime.fromtimestamp(reply_float_3)
+      t_server_3_corrected = t_server_3 + timedelta(seconds=(rtt_3/2))
+
+      t_send_4 = datetime.now()
+      s.sendto(msg.encode('utf-8'), (host4, port))
+      data_4 = s.recvfrom(1024) 
+      t_recv_4 = datetime.now()
+      t_round_4 = t_recv_4 - t_send_4
+      rtt_4 = t_round_4.total_seconds()
+      reply_float_4 = float(data_4[0].decode("utf-8"))
+      t_server_4 = datetime.fromtimestamp(reply_float_4)
+      t_server_4_corrected = t_server_4 + timedelta(seconds=(rtt_4/2))
+
+      t_send_5 = datetime.now()
+      s.sendto(msg.encode('utf-8'), (host5, port2))
+      data_5 = s.recvfrom(1024) 
+      t_recv_5 = datetime.now()
+      t_round_5 = t_recv_5 - t_send_5
+      rtt_5 = t_round_5.total_seconds()
+      reply_float_5 = float(data_5[0].decode("utf-8"))
+      t_server_5 = datetime.fromtimestamp(reply_float_5)
+      t_server_5_corrected = t_server_5 + timedelta(seconds=(rtt_5/2))
+
+      ######## Outputs ########
+
+      print("Current (local) time: " + str(t_send_1) )
+      print('Server 1 time       : ' + str(t_server_1) )
+      print("RTT to server1 (sec): " + str(rtt_1) )
+      print("Diff 1 (sec)        : " + str( (t_send_1 - t_server_1_corrected).total_seconds() ) +'\n')
+
+      print("Current (local) time: " + str(t_send_2) )
+      print('Server 2 time       : ' + str(t_server_2) )
+      print("RTT to server2 (sec): " + str(rtt_2) )
+      print("Diff 2 (sec)        : " + str( (t_send_2 - t_server_2_corrected).total_seconds() ) +'\n')
+
+      print("Current (local) time: " + str(t_send_3) )
+      print('Server 3 time       : ' + str(t_server_3) )
+      print("RTT to server3 (sec): " + str(rtt_3) )
+      print("Diff 3 (sec)        : " + str( (t_send_3 - t_server_3_corrected).total_seconds() ) +'\n')
+
+      print("Current (local) time: " + str(t_send_4) )
+      print('Server 4 time       : ' + str(t_server_4) )
+      print("RTT to server4 (sec): " + str(rtt_4) )
+      print("Diff 4 (sec)        : " + str( (t_send_4 - t_server_4_corrected).total_seconds() ) +'\n')
+
+      print("Current (local) time: " + str(t_send_5) )
+      print('Server 5 time       : ' + str(t_server_5) )
+      print("RTT to server5 (sec): " + str(rtt_5) )
+      print("Diff 5 (sec)        : " + str( (t_send_5 - t_server_5_corrected).total_seconds() ) +'\n')
+
+      print("******************************\n")
     # some problem seding data?
     except socket.error as e:
       print ('Error Code: ' + str(e[0]) + ' Message ' + e[1])
@@ -65,4 +124,9 @@ def sync():
 
 if __name__ == "__main__":
   sync()
+  print("***** Marzullo *****")
+  test_list = ( (-13.0,-11.0),(-6.2,-6.0),(-2.4,-2.2),(4.8,5.0),(6.0,6.4))
+  result = marzullo.marzullo_algorithm(test_list)
+  print(result)
+
   print('Program Complete')
--- hw1/marzullo.py
+++ hw1/marzullo.py
@@ -1,41 +1,46 @@
 # c.f. http://en.wikipedia.org/wiki/Marzullo%27s_algorithm
 def marzullo_algorithm(ranges):
-    table = []
-    for l,r in ranges:
-        table.append((l,-1))
-        table.append((r,+1))
+  table = []
+  for l,r in ranges:
+    table.append((l,-1))
+    table.append((r,+1))
 
-    def my_cmp(x, y):
-        result = cmp(x[0], y[0])
-        if result == 0:
-            result = -cmp(x[1], y[1]) # to exclude 'pathological overlaps'
-        return result
-    table.sort(my_cmp)
+  def my_cmp(x, y):
+    result = cmp(x[0], y[0])
+    if result == 0:
+      result = -cmp(x[1], y[1]) # to exclude 'pathological overlaps'
+    return result
+  table.sort(my_cmp)
 
-    best = 0
-    cnt = 0
-    for i in range(len(table) - 1):
-        cnt = cnt - table[i][1]
-        if best < cnt:
-            best = cnt
-            beststart = table[i][0]
-            bestend   = table[i+1][0]
-    return (beststart, bestend)
-        
+  best = 0
+  cnt = 0
+  for i in range(len(table) - 1):
+    cnt = cnt - table[i][1]
+    if best <= cnt:
+      best = cnt
+      beststart = table[i][0]
+      bestend   = table[i+1][0]
+      
+  
+  return (beststart, bestend)
+    
 def test(data_list):
-    for data in data_list:
-        result = marzullo_algorithm(data['input']) 
-        if not result == data['expected']:
-            print 'test failed input=', data['input'], 'expected=', data['expected'], 'actual=', result
-            return
-    print 'test suceeded'
+  for data in data_list:
+    result = marzullo_algorithm(data['input']) 
+    if not result == data['expected']:
+      print 'test failed input=', data['input'], 'expected=', data['expected'], 'actual=', result
+      return
+  print 'test suceeded'
 
 if __name__ == "__main__":
-    test_data_list = (
-            {'input' : ((8,12),(11,13),(10,12)), 'expected' : (11,12)},
-            {'input' : ((8,12),(11,13),(14,15)), 'expected' : (11,12)},
-            {'input' : ((8,9),(8,12),(10,12)), 'expected' : (8,9)},
-            {'input' : ((8,12),(9,10),(11,13),(10,12)), 'expected' : (11,12)},
-            {'input' : ((8,12),(9,10),(11,13),(14,15)), 'expected' : (9,10)},
-            {'input' : ((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15)), 'expected' : (12,13)})
-    test(test_data_list)
+  test_data_list = (
+      {'input' : ((8,12),(11,13),(10,12)), 'expected' : (11,12)},
+      {'input' : ((8,12),(11,13),(14,15)), 'expected' : (11,12)},
+      {'input' : ((8,9),(8,12),(10,12)), 'expected' : (8,9)},
+      {'input' : ((8,12),(9,10),(11,13),(10,12)), 'expected' : (11,12)},
+      {'input' : ((8,12),(9,10),(11,13),(14,15)), 'expected' : (9,10)},
+      {'input' : ((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15)), 'expected' : (12,13)})
+  test(test_data_list)
+  test_list = ( (-13.0,-11.0),(-6.2,-6.0),(-2.4,-2.2),(4.8,5.0),(6.0,6.4))
+  result = marzullo_algorithm(test_list)
+  print(result)
